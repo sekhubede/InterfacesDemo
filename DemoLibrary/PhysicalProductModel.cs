70d9ba20b589ac43dd9c8062e1a1078bfd365d81
sekhubede <43635891+sekhubede@users.noreply.github.com>
2023-03-22 16:40:09 +0200

    Added properties and a ShipItem method
    ShipItem method just simulates the shipment of an item.

--- DemoLibrary/PhysicalProductModel.cs
+++ DemoLibrary/PhysicalProductModel.cs
@@ -4,9 +4,22 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using static System.Console;
+
 namespace DemoLibrary
 {
     public class PhysicalProductModel
     {
+        public string Title { get; set; }
+        public bool HasOrderBeenCompleted { get; private set; }
+
+        public void ShipItem(CustomerModel customer)
+        {
+            if (HasOrderBeenCompleted == false) 
+            {
+                WriteLine($"Simulating shipping {Title} to {customer.FirstName} in {customer.City}");
+                HasOrderBeenCompleted = true;
+            }
+        }
     }
 }
