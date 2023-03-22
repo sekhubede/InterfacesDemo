e89a7d21fc9ac07e9934f9cff1bf2ae68d245544
sekhubede <43635891+sekhubede@users.noreply.github.com>
2023-03-22 16:46:32 +0200

    created a DemoProductModel class and implemented some logic to it

---
+++ DemoLibrary/DigitalProductModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using static System.Console;
+
+namespace DemoLibrary
+{
+    public class DigitalProductModel
+    {
+        public string Title { get; set; }
+
+        public void ShipItem(CustomerModel customer)
+        {
+            WriteLine($"Simulating emailing {Title} to {customer.EmailAddress}");
+        }
+    }
+}
