3ff47cac36252c064a9b9b8ea570e441767dff80
sekhubede <43635891+sekhubede@users.noreply.github.com>
2023-03-23 09:55:41 +0200

    Created a base vehicle class

---
+++ DemoLibrary2/Vehicle.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoLibrary2
+{
+    // Base class for Vehicles.
+    public class Vehicle
+    {
+        // Speed of the vehicle.
+        public float Speed { get; set; }
+
+        // Color of the vehicle.
+        public string Color { get; set; }
+
+        // Default constructor.
+        public Vehicle()
+        {
+            Speed = 120f;
+            Color = "White";
+        }
+
+        // Simple constructor.
+        public Vehicle(float speed, string color)
+        {
+            Speed = speed;
+            Color = color;
+        }
+    }
+}
