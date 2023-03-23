217f525fe844f932dcb49fe05fb9e218cdead56c
sekhubede <43635891+sekhubede@users.noreply.github.com>
2023-03-23 09:56:23 +0200

    Created a base furniture class

---
+++ DemoLibrary2/Furniture.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoLibrary2
+{
+    // Base class for furnitures.
+    public class Furniture
+    {
+        // Color of the furniture.
+        public string Color { get; set; }
+
+        // Material of the furniture.
+        public string Material { get; set; }
+
+        // Default constructor.
+        public Furniture() 
+        {
+            Color = "White";
+            Material = "Wood";
+        }
+
+        // Simple constructor.
+        public Furniture(string color, string matrial)
+        {
+            Color = color;
+            Material = matrial;
+        }
+    }
+}
