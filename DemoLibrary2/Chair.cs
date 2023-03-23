baae4115ed68f9d2887a65f4a627cbd3add3fafc
sekhubede <43635891+sekhubede@users.noreply.github.com>
2023-03-23 09:56:52 +0200

    Created a sublclass chair class that extends the furniture class

---
+++ DemoLibrary2/Chair.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.PortableExecutable;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoLibrary2
+{
+    // Subclass chair that extends Furniture.
+    public class Chair : Furniture
+    {
+        // Simple constructor.
+        public Chair (string color, string material) : base (color, material) 
+        { 
+         
+        }
+    }
+}
