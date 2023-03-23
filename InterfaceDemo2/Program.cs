5076f9aa8791673222e1c7c3e42e95328ec94271
sekhubede <43635891+sekhubede@users.noreply.github.com>
2023-03-23 10:55:41 +0200

    Implemented the logic to InterfaceDemo2

--- InterfaceDemo2/Program.cs
+++ InterfaceDemo2/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using DemoLibrary2;
+using static System.Console;
 
 namespace InterfaceDemo2
 {
@@ -6,7 +8,23 @@ namespace InterfaceDemo2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Creating two objects of type chair.
+            Chair officeChair = new Chair("Brown","Plastic");
+            Chair gamingChair = new Chair("Red","Wood");
+
+            // Creating a new object of type car.
+            Car damagedCar = new Car(80,"Blue");
+
+            // Add the two chairs to the IDestrotable list of the car
+            // so that when we destroy the car the destroyable objects
+            // that are near the car will get destroyed as well.
+            damagedCar.DestroyablesNearby.Add(officeChair);
+            damagedCar.DestroyablesNearby.Add(gamingChair);
+
+            damagedCar.Destroy();
+
+            WriteLine("\nPress any key to exit...");
+            ReadKey(true);
         }
     }
 }
