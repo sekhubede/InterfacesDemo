676d523a8e8ff1817a070bde94014d18cdae6433
sekhubede <43635891+sekhubede@users.noreply.github.com>
2023-03-23 09:56:10 +0200

    Created a sublclass car that extends the vehicle class

---
+++ DemoLibrary2/Car.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoLibrary2
+{
+    // Subclass Car that extends Vehicle
+    public class Car : Vehicle
+    {
+        // Simple constructor.
+        public Car(float speed, string color) : base (speed, color)
+        {
+
+        }
+    }
+}
