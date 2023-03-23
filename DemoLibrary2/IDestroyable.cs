e5f04996a1983ebed1d276dfc74508eec07bc9c6
sekhubede <43635891+sekhubede@users.noreply.github.com>
2023-03-23 10:01:36 +0200

    Created an IDestroyable interface class

---
+++ DemoLibrary2/IDestroyable.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoLibrary2
+{
+    public interface IDestroyable
+    {
+        // Property to store the audio file of the destruction sound.
+        string DestructionSound { get; set; }
+
+        // Method to destroy an object.
+        void Destroy();
+    }
+}
